a9e3ea0de9c64b5a0bdca1fbca6ab04df9d06cc3
Kartikey Semwal <semwal.kartikey999@gmail.com>
2023-10-17 21:42:18 +0530

    writing listener for RabbitMQ

---
+++ go-micro/listener-service/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// to rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	log.Println("Connected to RabbitMQ")
+
+	// listen to queue
+
+	// create consumer
+
+	// consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("Backing off")
+
+		time.Sleep(backoff)
+	}
+
+	return connection, nil
+}
